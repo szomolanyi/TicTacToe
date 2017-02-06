d2875124c9aba36bd595eb164f90a970ffcf3ce8
szomolanyi <szomolanyi@gmail.com>
2017-02-06 13:20:56 +0100

    init

--- src/index.js
+++ src/index.js
@@ -2,10 +2,49 @@
 
 require("./styles/style.css");
 require("jquery");
+require("font-awesome-webpack");
 
 // Main app
+function dbg(msg) {
+  let p=$('<p>').html(msg);
+  $('#debug').append(p);
+}
+
+let state={
+  debugOn: false,
+  onTurn: 'none', /* who is on turn : none, 0 or x */
+  handleTurn: function(id) {
+    let x=(id-1)%3;
+    let y=Math.trunc((id-1)/3);
+
+    if (this.onTurn === '0') {
+      this.onTurn='x';
+    }
+    else {
+      this.onTurn='0';
+    }
+    dbg(`Turn on ${x}, ${y} id=${id} nextTurn=${this.onTurn}`);
+  }
+};
 
 var f = param => console.log(param);
 
-document.write('Hello fe base !');
 f('es 6 working :)');
+
+function turn_debug() {
+  if (state.debugOn) {
+    $('#debug').removeClass('pop');
+    state.debugOn=false;
+  }
+  else {
+    $('#debug').addClass('pop');
+    state.debugOn=true;
+  }
+}
+
+$(document).ready(function () {
+  $('#debug').click(turn_debug);
+  $('td').click(function () {
+    state.handleTurn(this.id);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,9 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loaders: ['eslint-loader', 'babel-loader?presets[]=es2015']
-            }
+            },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
     },
     plugins: [
